725d90cdef268b2ab817c3f67ac9f8f7dee6291f
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-11-02 23:40:08 +0330

    feat: add link to each book item found

--- src/app/_components/book-search-item/book-search-item.tsx
+++ src/app/_components/book-search-item/book-search-item.tsx
@@ -1,18 +1,26 @@
 "use client";
 import { Book } from "@/hooks/useBooksQuery";
 import useSearchQuery from "@/hooks/useSearchQuery";
-import { Autocomplete, AutocompleteItem, Spinner } from "@nextui-org/react";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Avatar,
+  Link,
+  Spinner,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export const BookSearchItem: React.FC = () => {
   const [search, setSearch] = useState("");
   const { data, isLoading, status } = useSearchQuery(search);
-
+  const router = useRouter();
   return (
     <Autocomplete
       onValueChange={(value: string) => setSearch(value)}
-      label="Select an animal"
-      className="max-w-xs"
+      label="Select an book"
+      className="max-w-full"
+      items={data}
     >
       {isLoading ? (
         <AutocompleteItem key="loading">
@@ -25,7 +33,13 @@ export const BookSearchItem: React.FC = () => {
       ) : status === "success" ? (
         data!.map((item: Book) => (
           <AutocompleteItem key={item.id} value={item.title}>
-            {item.title}
+            <Link onPress={() => router.push(`/book/${item.id}`)}>
+              <div className="flex justify-between justify-items-center items-center">
+                <Avatar src={item.img} isBordered radius="sm" size="sm" />
+                <span>{item.title}</span>
+                <span>{item.rating}</span>
+              </div>
+            </Link>
           </AutocompleteItem>
         ))
       ) : (
