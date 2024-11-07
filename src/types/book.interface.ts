4b1e55876384909e12e5d0ca1e6f92cf922e2c76
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-11-07 22:39:48 +0330

    feat: add SearchTerm interface

--- src/types/book.interface.ts
+++ src/types/book.interface.ts
@@ -4,3 +4,12 @@ export interface Book {
   img: string;
   rating: number;
 }
+
+export interface SearchTerm {
+  param_search_term?: string;
+  param_publish_date?: number;
+  param_rating?: number;
+  param_pages?: number;
+  param_genre?: string;
+  param_bookFormat?: string;
+}
