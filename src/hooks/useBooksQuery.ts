1d383dfbb46a846c10bf9cfddefed8bd08fd698c
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-11-08 11:51:15 +0330

    refactor: remove book interface from useBooksQuery custom hook

--- src/hooks/useBooksQuery.ts
+++ src/hooks/useBooksQuery.ts
@@ -1,19 +1,15 @@
 import { API_KEY, API_URL } from "@/configs/globals";
 import { readData } from "@/core/http-service";
+import { Book } from "@/types/book.interface";
 import { useQuery } from "@tanstack/react-query";
 import { RawAxiosRequestHeaders } from "axios";
-export interface Book {
-  id: number;
-  title: string;
-  img: string;
-  rating: number;
-}
+
 const getBooks = async () => {
   const url = `${API_URL}Books?select=id,title,img,rating`;
   const headers: RawAxiosRequestHeaders = {
     Authorization: `Bearer ${API_KEY}`,
     apiKey: API_KEY,
-    range: "0-9",
+    range: "0-30",
   };
   const res = await readData<Book[]>(url, headers);
   return res;
